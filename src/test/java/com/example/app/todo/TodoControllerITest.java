09b202fb18e244cdc5521b5aa074f548f4cfc76a
Chris Martine <cmmartine@gmail.com>
2025-05-22 14:34:48 -0400

    Initialize project, add TodoController and tests

---
+++ src/test/java/com/example/app/todo/TodoControllerITest.java
@@ -0,0 +1,27 @@
+package com.example.app.todo;
+
+import com.example.app.Application;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// Integration test that does not use MockMvc
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TodoControllerITest {
+  
+  @Autowired
+  private TestRestTemplate template;
+
+  @Test
+  public void getTodo() throws Exception {
+    ResponseEntity<String> response = template.getForEntity("/", String.class);
+    assertThat(response.getStatusCodeValue()).isEqualTo(200);
+    assertThat(response.getBody()).isEqualTo("Greetings from Spring Boot TodoController!");
+  }
+}
---
+++ src/test/java/com/example/app/todo/TodoControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.app.todo;
+
+import com.example.app.Application;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+public class TodoControllerTest {
+  
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void getTodo() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(content().string(equalTo("Greetings from Spring Boot TodoController!")));
+  }
+}
